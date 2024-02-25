31d2f10fd263450c9e82a68540e368646af0dea9
Alexey Minimulin <a.s.minimulin@gmail.com>
2024-02-25 21:20:42 +0300

    Do not include debug info into release build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 const path = require('path')
 
-module.exports = {
-    entry: './app/index.jsx',
-    output: {
-        filename: 'miniapp.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.jsx', '.ts', '.js'],
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: "./static",
-        port: 9000,
-    },
-};
+module.exports = function (env, argv) {
+
+	const cfg = {
+        entry: './app/index.jsx',
+        output: {
+            filename: 'miniapp.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.jsx', '.ts', '.js'],
+        },
+        devtool: env.development ? 'inline-source-map' : false,
+        devServer: {
+            static: "./static",
+            port: 9000,
+        },
+    }
+    return cfg;
+}
