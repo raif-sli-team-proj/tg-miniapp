c097dd18879f3712d0c76a30673e93700d03cb97
Alexey Minimulin <a.s.minimulin@gmail.com>
2024-03-20 03:30:51 +0300

    Update config.json, add all known services

--- app/config.js
+++ app/config.js
@@ -7,11 +7,17 @@ config.services = [
     'Информация о платеже',
     'Внесение ДС',
     'Управление чеками',
+    'Проведение платежей',
+    'Получение списка операций',
 ];
 config.serviceNames = [
     "QRC",
     "MRI",
     "PYI",
+    "payments_processing",
+    "receipts-servce",
+    "Payments",
+    "operations"
 ];
 config.incidents_page_size = 20;
 config.failed_request_retry_period = 10;  // in seconds
